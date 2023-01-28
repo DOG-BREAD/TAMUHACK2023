c62f59fcffc858c1f27d031300db8aaa5aabbebc
DOG-BREAD <samueljacobsbuisness@gmail.com>
2023-01-28 15:44:11 -0600

    ok
    main fikles

---
+++ CBRE/src/cbre/CBRE.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cbre;
+
+/**
+ *
+ * @author theje
+ */
+import java.io.*;
+import java.util.*;
+public class CBRE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    public String ROOMS(){
+    String floors[] = {"A","B","C","D","E"};
+    int floorSpace[] = {43,81,73,54,97};
+    int floorSpaceFilled[] = {0,0,0,0,0};
+    int teams[] = {22,45,34,51,11,37,42,16,29,56,49};
+    boolean teamsFilled[] = new boolean[11];
+    int i=0;
+    for( i = 0; i < teamsFilled.length; i++){
+        teamsFilled[i] = false;
+    }
+    //int prefferences;
+    //while(){
+    
+    //}
+    return "";
+}
+}
+
