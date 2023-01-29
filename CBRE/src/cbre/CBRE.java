2e9fe2358e191a23405c0cbda1d2ebb5c0d35ba8
Ibad Momin <ibadalimomin@gmail.com>
2023-01-28 23:42:18 -0600

    scanned teams, strength, and preferences

--- CBRE/src/cbre/CBRE.java
+++ CBRE/src/cbre/CBRE.java
@@ -16,9 +16,55 @@ public class CBRE {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Integer> teams = new ArrayList<Integer>();
+        ArrayList<Integer> strengths = new ArrayList<Integer>();
+        ArrayList<ArrayList<Integer>>  preferred = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>>  tolerated = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>>  noWay = new ArrayList<ArrayList<Integer>>();
+
+        File files = new File("/Users/ibadmomin/Desktop/TAMUHACK/CBRE/src/cbre/data.txt");
+        Scanner scan = new Scanner(files);
+
+        while (scan.hasNext()){
+            String s = "";
+            for (int i=0; i<5; i++){
+                s += scan.nextLine() + "\n";
+            }
+            Scanner lineScan = new Scanner(s);
+            int team = lineScan.nextInt();
+            int people = lineScan.nextInt();
+            lineScan.nextLine();
+            String prefTeamString = lineScan.nextLine();
+            prefTeamString = prefTeamString.replace(" ", "");
+            String[] prefTeamArr = prefTeamString.split(",");
+            int[] prefTeams = new int[prefTeamArr.length];
+            String tolTeamString = lineScan.nextLine();
+            tolTeamString = tolTeamString.replace(" ", "");
+            String[] tolTeamArr = tolTeamString.split(",");
+            int[] tolTeams = new int[tolTeamArr.length];
+            String noWayString = lineScan.nextLine();
+            noWayString = noWayString.replace(" ", "");
+            String[] noWayArr = noWayString.split(",");
+            int[] noWayTeams = new int[noWayArr.length];
+
+            //converts string of preferred teams to int
+            for (int i=0; i<prefTeams.length; i++){
+                prefTeams[i] = Integer.parseInt(prefTeamArr[i]);
+            }
+            for (int i=0; i<tolTeams.length; i++){
+                tolTeams[i] = Integer.parseInt(tolTeamArr[i]);
+            }
+            for (int i=0; i<noWayTeams.length; i++){
+                noWayTeams[i] = Integer.parseInt(noWayArr[i]);
+            }
+            System.out.println("team: "+ team);
+            System.out.println("people: "+ people);
+            System.out.println("preferred: "+ Arrays.toString(prefTeams));
+            System.out.println("tolerated: "+ Arrays.toString(tolTeams));
+            System.out.println("no way: "+ Arrays.toString(noWayTeams));
+
+        }
     }
     public String ROOMS(){
     String floors[] = {"A","B","C","D","E"};
@@ -30,10 +76,6 @@ public class CBRE {
     for( i = 0; i < teamsFilled.length; i++){
         teamsFilled[i] = false;
     }
-    //int prefferences;
-    //while(){
-    
-    //}
     return "";
 }
 }
